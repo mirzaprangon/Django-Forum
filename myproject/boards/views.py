0db54d37b6918ece10c27277fdc6e363f619f04a
Mirza Prangon <mirza.prangon@gmail.com>
2018-08-04 00:06:48 +0600

    First commit
    Static pages working with links working to topics.

---
+++ myproject/boards/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, get_object_or_404
+
+# Create your views here.
+from django.http import HttpResponse, Http404
+from .models import Board
+
+
+def home(request):
+    boards = Board.objects.all()
+    '''boards_names = list()
+
+    for board in boards:
+        boards_names.append(board.name)
+
+
+    response_html = '<br>'.join(boards_names)
+    return HttpResponse(response_html)'''
+    return render(request, 'home.html', {'boards': boards})
+
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board': board})
+
